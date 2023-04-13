9fc791da59f2c3ccdaeeb7356a3362e14ccfeaed
Kita Igniter <smtecsystem.469@gmail.com>
2023-04-12 22:42:50 -0300

    chorus!: setting eslint

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
-const app = fastify();
+const app = fastify()
 
-app.get("/hello", (req, res) => {
-  return res.send("Hello World!");
-});
+app.get('/hello', (req, res) => {
+  return res.send('Hello World!')
+})
 
-app.listen({
-  port: 3333,
-}).then(() => {
-  console.log("Running on port 3333 🚀")
-});
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('Running on port 3333 🚀')
+  })
