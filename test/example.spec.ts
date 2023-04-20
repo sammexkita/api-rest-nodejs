78ee4b7fce6ad3e976c676e6db1ad21f67b84fd6
Kita Igniter <smtecsystem.469@gmail.com>
2023-04-20 06:38:27 -0300

    api!: Tests Category

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,22 +1,24 @@
-import { afterAll, beforeAll, test } from 'vitest'
+import { afterAll, beforeAll, describe, it } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 
-beforeAll(async () => {
-  await app.ready()
-})
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
 
-afterAll(async () => {
-  await app.close()
-})
+  afterAll(async () => {
+    await app.close()
+  })
 
-test('O usuário consegue criar uma nova transação', async () => {
-  await request(app.server)
-    .post('/transactions')
-    .send({
-      title: 'New transaction',
-      amount: 5000,
-      type: 'credit',
-    })
-    .expect(201)
+  it('should be able to create a new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
 })
